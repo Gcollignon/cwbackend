b78a6a6bf811c597e785799e1074196c50c33bea
gailen <gcollignon@gereeco.com>
2023-11-23 19:34:43 +0100

    First dev public version

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ const dev_router = require('./routes/dev')
 const malicious_router = require('./routes/malicious')
 const wr_router = require('./routes/wr');
 const config = require('../config.json')
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use('/api/coleweight/', coleweight_router)
 app.use('/dev/', dev_router);
 app.use('/api/malicious/', malicious_router);
 app.use('/api/wr/', wr_router);
-app.listen(8080, async () => {
+app.listen(3031, async () => {
   console.log('Server is running.')
   await mongoose.connect(config.mongodb);
 

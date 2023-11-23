9268bc173bdda8d0d33a81f9601868d8dac6ffbf
gailen <gcollignon@gereeco.com>
2023-11-23 12:39:12 +0100

    Changed routes in index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const malicious_router = require('./routes/malicious')
 const wr_router = require('./routes/wr');
 
 const app = express();
-app.use('/api/', coleweight_router)
+app.use('/api/coleweight/', coleweight_router)
 app.use('/dev/', dev_router);
-app.use('/malicious/', malicious_router);
-app.use('/wr/', wr_router);
+app.use('/api/malicious/', malicious_router);
+app.use('/api/wr/', wr_router);
 app.listen(3000, async () => {
   console.log('Server is running.')
   await mongoose.connect('mongodb://127.0.0.1:27017/cw1');
